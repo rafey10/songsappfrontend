4b8cc032243bd8d3789abbc027d89bae7ec11c9d
Raja Abdur Rafey <raja.rafey@futurice.com>
2024-05-17 00:23:25 +0200

    CORS access fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [apiResponse, setApiResponse] = useState(String);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://b25hr3zed5.execute-api.eu-north-1.amazonaws.com/songsappdeploymentstage/hello-world",
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+          },
+        }
+      )
+      .then((response) => setApiResponse(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +29,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{apiResponse}</p>
         <a
           className="App-link"
           href="https://reactjs.org"

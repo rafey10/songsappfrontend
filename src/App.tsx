c66a70cfb3940e202860c448b483d00cc3110ab7
Raja Abdur Rafey <raja.rafey@futurice.com>
2024-05-17 23:00:09 +0200

    Test FE with dummy Api calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [apiResponse, setApiResponse] = useState(String);
+  useEffect(() => {
+    axios
+      .post("https://b25hr3zed5.execute-api.eu-north-1.amazonaws.com/songsappdeploymentstage/add-song", {
+        song: "NewArtist",
+        artist: "NewArtist",
+        album: "NewArtist",
+        releaseYear: "2019",
+        length: "11:08:59",
+        genre: "NewGenre",
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }, []);
 
   useEffect(() => {
     axios
       .get(
-        "https://b25hr3zed5.execute-api.eu-north-1.amazonaws.com/songsappdeploymentstage/hello-world"
+        "https://b25hr3zed5.execute-api.eu-north-1.amazonaws.com/songsappdeploymentstage/get-songs"
       )
-      .then((response) => setApiResponse(response.data))
+      .then((response) => console.log(response))
       .catch((error) => console.log(error));
   }, []);
 
@@ -22,7 +34,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>{apiResponse}</p>
+        {/* <p>{apiResponse}</p>
+        <p>{apiResponse2}</p> */}
         <a
           className="App-link"
           href="https://reactjs.org"

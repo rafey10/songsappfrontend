bbf0dfa4283595cd066cfb6738b91002dd0c8bcd
Raja Abdur Rafey <raja.rafey@futurice.com>
2024-05-18 04:13:40 +0200

    Call Api

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,18 @@ import axios from "axios";
 function App() {
   useEffect(() => {
     axios
-      .post("https://b25hr3zed5.execute-api.eu-north-1.amazonaws.com/songsappdeploymentstage/add-song", {
-        song: "NewArtist",
-        artist: "NewArtist",
-        album: "NewArtist",
-        releaseYear: "2019",
-        length: "11:08:59",
-        genre: "NewGenre",
-      })
+      .post(
+        "https://b25hr3zed5.execute-api.eu-north-1.amazonaws.com/songsappdeploymentstage/add-song",
+        // "http://localhost:8080/add-song",
+        {
+          song: "NewestSong",
+          artist: "NewArtist",
+          album: "NewArtist",
+          releaseYear: "2019",
+          length: "11:08:59",
+          genre: "NewGenre",
+        }
+      )
       .then((response) => console.log(response))
       .catch((error) => console.log(error));
   }, []);
@@ -22,6 +26,7 @@ function App() {
     axios
       .get(
         "https://b25hr3zed5.execute-api.eu-north-1.amazonaws.com/songsappdeploymentstage/get-songs"
+        // "http://localhost:8080/get-songs"
       )
       .then((response) => console.log(response))
       .catch((error) => console.log(error));
